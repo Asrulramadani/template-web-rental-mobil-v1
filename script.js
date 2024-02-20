77fb5a41a874b3c73e10bc0004488d8c7266f50f
Asrul Ramadani <asrulramadani0711@gmail.com>
2024-02-21 01:50:29 +0800

    minor fixed on detail-pemesanan page

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const btnPesan = document.querySelector("#btn-pesan");
 const formCekKetersediaan = document.querySelector("#cek-ketersediaan");
+const formPesan = document.querySelector("#pesan");
 const alert = document.querySelector(".alert");
 
 btnPesan.addEventListener("click", (e) => {
   e.preventDefault();
   formCekKetersediaan.classList.add("d-none");
+  formPesan.classList.remove("d-none");
   alert.classList.add("d-none");
 });
